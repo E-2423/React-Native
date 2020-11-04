107a54f5b57a1dbf0c6b2249940c0fce8e1c863b
Joseph Abraham <ekurubal24@gmail.com>
2020-11-04 15:53:22 +0300

    created index.js and jobs.js

---
+++ Portfolio_1/src/pages/Jobs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const Jobs = (props) => {
+  
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>JOBS</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export {Jobs};
