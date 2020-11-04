00cd30e78496aa6e83d4b64340260a8aa35154c2
Joseph Abraham <ekurubal24@gmail.com>
2020-11-04 22:31:55 +0300

    feature:added modal and buttom

--- Portfolio_1/src/pages/Jobs.js
+++ Portfolio_1/src/pages/Jobs.js
@@ -1,11 +1,15 @@
 import Axios from 'axios';
+import Modal from 'react-native-modal';
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Text, View, FlatList} from 'react-native';
+import {SafeAreaView, Text, View, FlatList, Button} from 'react-native';
 
+import {jobs} from '../styles'
 import {JobItem} from '../components';
 
 const Jobs = (props) => {
   const [data, setData] = useState([]);
+  const [selectedJob, setSelectedJob] = useState("");
+  const [modalFlag, setModalFlag] = useState(false);
   const {selectedLanguage} = props.route.params;
 
   const fetchData = async () => {
@@ -19,7 +23,13 @@ const Jobs = (props) => {
     fetchData();
   }, []);
 
-  const renderJobs = ({item}) => <JobItem job={item} />;
+  const onJobSelect = (job) =>{
+    setModalFlag(true)
+    setSelectedJob(job)
+
+  }
+
+  const renderJobs = ({item}) => <JobItem job={item} onSelect={()=> onJobSelect(item)}/>;
 
   return (
     <SafeAreaView>
@@ -30,6 +40,21 @@ const Jobs = (props) => {
         fontSize: 20
       }}>JOBS FOR {selectedLanguage.toUpperCase()}</Text>
         <FlatList data={data} renderItem={renderJobs} />
+        <Modal isVisible={modalFlag} onBackdropPress={()=>setModalFlag(false)}>
+          <View style={jobs.modalBackground}>
+            <View style={{borderBottomWidth:2, borderColor:'#bdbdbd'}}>
+              <Text style={jobs.jobTitle}>{selectedJob.title}</Text>
+              <Text>{selectedJob.location}/ {selectedJob.title}</Text>
+              <Text>{selectedJob.company}</Text>
+            </View>
+            <View style={jobs.jobDesc}>
+              <Text numberOfLines={5}>{selectedJob.description}</Text>
+            </View>
+            <Button
+              title='Kaydet' onPress={()=> null}
+            />
+          </View>
+        </Modal>
       </View>
     </SafeAreaView>
   );
