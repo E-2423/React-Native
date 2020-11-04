8ad194ced3445e5712877ca7644aa15880d5a2ed
Joseph Abraham <ekurubal24@gmail.com>
2020-11-05 00:29:34 +0300

    completed project

--- Portfolio_1/src/components/TopicItem.js
+++ Portfolio_1/src/components/TopicItem.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import {TouchableOpacity, Text} from 'react-native';
 
-import {topicItem} from '../styles';
+import { topicItem } from '../styles';
+
+const TopicItem = (props) => {
+  return (
+      <TouchableOpacity
+        style={[topicItem.container, { backgroundColor: `#${props.item.color}` }]}
+        onPress={props.onSelect}
+      >
+          <Text style={topicItem.text}>{props.item.name}</Text>
+      </TouchableOpacity>
+    );
+};
+
+export {TopicItem};
+
 
 // {
 //     id: 0,
 //     name: 'Java',
 //     color: 'fb5607'
 //   },
-
-const TopicItem = (props) =>{
-    return (
-        <TouchableOpacity 
-            style={[topicItem.container, {backgroundColor:`#${props.item.color}`}]}
-            onPress={props.onSelect}
-        >
-            <Text style={topicItem.text}>{props.item.name}</Text>
-        </TouchableOpacity>
-    );
-
-    
-};
-
-export {TopicItem};
